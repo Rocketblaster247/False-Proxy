f9bdcb90ed701291628422efbd74dbce07966165
RocketBlaster <47755918+Rocketblaster247@users.noreply.github.com>
2019-08-10 15:40:36 -0400

    Update False-Proxy.js

--- False-Proxy.js
+++ False-Proxy.js
@@ -1,1 +1,13 @@
 console.log("Loaded False-Proxy...");
+var False_Proxy_XHR = XMLHttpRequest;
+var XMLHttpRequest = False_Proxy_XHR;
+XMLHttpRequest.prototype.FPopen = XMLHttpRequest.prototype.open;
+XMLHttpRequest.prototype.open = function (x, y, z, j, w) {
+  if (x === "POST") {
+    var com = false;
+    com = window.confirm("Block URL: " + y);
+    if (com) {
+      this.FPopen(x, y, z, j, w);
+    }
+  }
+};
