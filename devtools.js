88cf21bf4c0c50960e45b409ba97e25b3f26b824
RocketBlaster <47755918+Rocketblaster247@users.noreply.github.com>
2019-08-10 17:22:20 -0400

    Delete devtools.js

--- devtools.js
+++
@@ -1,8 +0,0 @@
-browser.devtools.panels.create(
-  "My Panel",                      // title
-  "icons/border-48.png",                // icon
-  "devtools/panel/panel.html"      // content
-).then((newPanel) => {
-  newPanel.onShown.addListener(initialisePanel);
-  newPanel.onHidden.addListener(unInitialisePanel);
-});
